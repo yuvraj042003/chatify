297192d515849233a82fb6c5c3ff1cccbd931aa8
yuvraj <yuvraj042003@gmail.com>
2023-06-11 12:24:36 +0530

    Now Commit

--- chatify/src/App.js
+++ chatify/src/App.js
@@ -1,22 +1,20 @@
 import socketIO from "socket.io-client"
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom' 
-import Join from "../src/Join/Join.js" 
+import Chat from "./Componant/Chat/chat"
+import Join from "./Componant/Join/Join" 
 const ENDPOINT='http://localhost:4500/'
 const socket = socketIO(ENDPOINT, {transports:['websocket']}) 
 
 function App() {
-
-  socket.on("connect",()=>{
-
-  })
-
-
-  return (
+ socket.on("connect",()=>{
+ })
+return (
     <>
     <BrowserRouter>
       <Routes>
       <Route extact path="/" element={<Join/>} />
+      <Route path="/chat" element={<Chat/>}/>
       </Routes>
     </BrowserRouter>
     </>
--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
-const http = require("http")
-const express = require('express')
-const cors = require("cors")
-const socketIO = require("socket.io")
+const http=require("http");
+const express =require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
 
 const app = express();
-const port = 4500 || process.env.PORT
+const users = [{}];
+port = 4500 || process.env.PORT
 
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("Hello Server is working")
+
+app.get('/', (req,res)=>{
+    res.send('hello This is working')
 })
 const server = http.createServer(app);
+const io=socketIO(server);
+io.on("connection",(socket)=>{
+    console.log("New Connection");
+    socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+    })
 
-const io = socketIO(server);
-io.on("connection",()=>{
-    console.log("This is new connection")
-})
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+        console.log(`${users[socket.id]} has left`);
+    })
+});
 server.listen(port,()=>{
-    console.log(`server is working on port ${port}`);
+    console.log(`Working`);
 })
+
+
+
+
+
 
